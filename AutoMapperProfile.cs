d343878d10a6cbf053dfb77dfe871360e2ab5cec
amornthep.c <amornthep.chuen@gmail.com>
2024-07-16 20:12:48 +0700

    Web API - AutoMapper

---
+++ AutoMapperProfile.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using DotNetRpg.Dtos.Character;
+
+namespace DotNetRpg;
+
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+        CreateMap<Character, GetCharacterDto>();
+        CreateMap<AddCharacterDto, Character>();
+    }
+}
