47880640d420a26f424ada797092fa13ebb2b807
amornthepc <amornthep.chuen@gmail.com>
2024-07-17 11:46:41 +0700

    Authentication - AuthenticationController

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetRpg.Data;
+using DotNetRpg.Dtos.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetRpg.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepo;
+
+        public AuthController(IAuthRepository authRepo)
+        {
+            _authRepo = authRepo;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto request)
+        {
+            var response = await _authRepo.Register
+            (
+                new User
+                {
+                    Username = request.Username,
+                },
+                request.Password
+            );
+
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+
+    }
+}
